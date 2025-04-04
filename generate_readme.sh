8733c191737e655f1de3dff05074269ab81640cb
quantumvoid <pranavrajesh009@gmail.com>
2025-04-04 18:55:19 +0530

    larger images

--- generate_readme.sh
+++ generate_readme.sh
@@ -3,17 +3,33 @@
 echo "Generating README.md..."
 echo "# Wallpapers" > README.md
 echo "" >> README.md
-
-# Add a div container for styling
-echo "<div style=\"display: grid; grid-template-columns: repeat(4, 1fr); gap: 10px;\">" >> README.md
+echo "<table>" >> README.md
 
 shopt -s nullglob
+count=0
+
 for img in wallpapers/*.{png,jpg,jpeg}; do
     if [[ -f "$img" ]]; then
-        echo "<div style=\"text-align: center;\"><img src=\"$img\" width=\"300px\"><br>$(basename "$img")</div>" >> README.md
+        # Start a new row every 3 images
+        if (( count % 3 == 0 )); then
+            echo "<tr>" >> README.md
+        fi
+
+        echo "<td style=\"text-align: center;\"><img src=\"$img\" width=\"300px\"><br>$(basename "$img")</td>" >> README.md
+
+        # Close the row after 3 images
+        if (( count % 3 == 2 )); then
+            echo "</tr>" >> README.md
+        fi
+
+        ((count++))
     fi
 done
 
-echo "</div>" >> README.md
+# If the last row isn't closed, close it
+if (( count % 3 != 0 )); then
+    echo "</tr>" >> README.md
+fi
 
+echo "</table>" >> README.md
 echo "README.md updated!"
